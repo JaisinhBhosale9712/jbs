dd6a31b060946f58e8d4a298a5edf6aebc519577
JaisinhBhosale9712 <71847281+JaisinhBhosale9712@users.noreply.github.com>
2022-02-07 01:36:02 +0100

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -2,7 +2,7 @@ import sys
 SECRET_KEY = "sfdwa87wq37"
 def fact(n):
     if n==0:
-      return 1
+        return 1
     return n*fact(n-1)
   
 def div(n):
@@ -16,3 +16,9 @@ def main(n):
 if __name__=="__main__":
     if len(sys.argv)>1:
         main(int(sys.argv[1]))
+class A:
+    def __mul__(self, other, unexpected):  # Noncompliant. Too many parameters
+        return 42
+
+    def __add__(self):  # Noncompliant. Missing one parameter
+        return 42
